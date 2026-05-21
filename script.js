e2997cb55498c514a709bff829ca051111075d5e
GUILHERME MARQUES SANTOS <99224826+droidmarx@users.noreply.github.com>
2026-05-21 12:49:31 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1050,7 +1050,7 @@ async function enviarTelegram(tipo) {
     
     // Redirect to Telegram after a short delay
     setTimeout(() => {
-      window.open('https://t.me/c/1989058391/167773', '_blank');
+      window.open('https://t.me/c/1989058391/167776', '_blank');
     }, 1500);
     
   } catch (error) {
